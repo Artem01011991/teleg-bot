e718b6d5152ed6fc0f0f0c87f09a5c4bfb5495f3
artem <you@example.com>
2018-11-25 21:48:02 +0300

    test request

--- bot/apps/last_ten_events/views.py
+++ bot/apps/last_ten_events/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+import requests
 
 
 def last_ten_events_view(request):
+    if request.POST['message']['text'] == '/feed':
+        requests.get(
+            'https://api.telegram.org/bot783030784:AAGA7zS4twvgcotcghkLPm-xPfrTn7h91SE/sendMessage',
+            params={'chat_id': request.POST['message']['chat']['id'], 'text': 'Hello'}
+        )
+
     return HttpResponse(status=204)
