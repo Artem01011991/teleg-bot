ad1033ab012bb1fd8a7be4d6d8bff1da647c45b5
artem <you@example.com>
2018-11-26 01:50:22 +0300

    requir

--- bot/apps/rest/views.py
+++ bot/apps/rest/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 from braces.views import CsrfExemptMixin
 import requests
 
@@ -7,6 +8,7 @@ import requests
 class Last10EventsView(CsrfExemptMixin, APIView):
     authentication_classes = []
 
+    @csrf_exempt
     def post(self, request):
         if request.method == 'POST':
             requests.get(
