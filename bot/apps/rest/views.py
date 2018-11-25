754748bb0d7c15b3bb5da8711dc216fd2a8bd56d
artem <you@example.com>
2018-11-26 01:18:06 +0300

    requir

--- bot/apps/rest/views.py
+++ bot/apps/rest/views.py
@@ -1,14 +1,17 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from .auth_classes import CsrfExemptSessionAuthentication
 import requests
 
 
-@api_view
-def last_10_events(request):
-    if request.method == 'POST':
-        requests.get(
-            'https://api.telegram.org/bot783030784:AAGA7zS4twvgcotcghkLPm-xPfrTn7h91SE/sendMessage',
-            params={'chat_id': request.POST['message']['chat']['id'], 'text': 'Hello'}
-        )
+class Last10EventsView(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication,)
 
-    return Response(status=204)
+    def post(self, request):
+        if request.method == 'POST':
+            requests.get(
+                'https://api.telegram.org/bot783030784:AAGA7zS4twvgcotcghkLPm-xPfrTn7h91SE/sendMessage',
+                params={'chat_id': request.POST['message']['chat']['id'], 'text': 'Hello'}
+            )
+
+        return Response(status=204)
--- bot/botproject/settings.py
+++ bot/botproject/settings.py
@@ -125,7 +125,6 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
