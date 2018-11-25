ca8f7dfdb53010b6cdb05f5ca5b2dc1ae9c220ff
artem <you@example.com>
2018-11-26 02:20:27 +0300

    requir

--- bot/botproject/settings.py
+++ bot/botproject/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     # 'django.middleware.security.SecurityMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'apps.rest.middleware.DisableCSRF'
     # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
