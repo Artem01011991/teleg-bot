ebfff38512a66cb7b741cf747e482da3f990ca18
artem <you@example.com>
2018-11-26 13:34:31 +0300

    precomlete changes

--- bot/apps/rest/views.py
+++ bot/apps/rest/views.py
@@ -7,9 +7,13 @@ class Last10EventsView(APIView):
     authentication_classes = []
 
     def post(self, request):
+        events = requests.get('https://test.bop.rest/api/feed/', headers={'Authorization': 'Token 233e7ef7888d82e098b3d63ca2a888d0e32a0eea'})
+
         requests.get(
             'https://api.telegram.org/bot783030784:AAGA7zS4twvgcotcghkLPm-xPfrTn7h91SE/sendMessage',
-            params={'chat_id': request.data['message']['chat']['id'], 'text': 'Hello'}
+            params={
+                'chat_id': request.data['message']['chat']['id'],
+                'text': ''.join('{time} {name}\n'.format(time=i['time'], name=i['name']) for i in events)}
         )
 
         return Response(status=204)
