ed8422707a4dc76a0a0927c4e61615942fc703c3
artem <you@example.com>
2018-11-26 13:19:32 +0300

    changes

--- bot/apps/last_ten_events/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LastTenEventsConfig(AppConfig):
-    name = 'last_ten_events'
--- bot/apps/last_ten_events/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import last_ten_events_view
-
-
-urlpatterns = [
-    path('', last_ten_events_view)
-]
