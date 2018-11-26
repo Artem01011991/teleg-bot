d8f3ffc53a4a97ad22ee1b36c31189717e890605
artem <you@example.com>
2018-11-26 13:11:27 +0300

    changes

--- bot/apps/rest/views.py
+++ bot/apps/rest/views.py
@@ -7,10 +7,9 @@ class Last10EventsView(APIView):
     authentication_classes = []
 
     def post(self, request):
-        print(request.data)
-        # requests.get(
-        #     'https://api.telegram.org/bot783030784:AAGA7zS4twvgcotcghkLPm-xPfrTn7h91SE/sendMessage',
-        #     params={'chat_id': request.POST['message']['chat']['id'], 'text': 'Hello'}
-        # )
+        requests.get(
+            'https://api.telegram.org/bot783030784:AAGA7zS4twvgcotcghkLPm-xPfrTn7h91SE/sendMessage',
+            params={'chat_id': request.data['message']['chat']['id'], 'text': 'Hello'}
+        )
 
         return Response(status=204)
